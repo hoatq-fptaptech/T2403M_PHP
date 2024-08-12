7fb357fd71b26d8cede883692ab96195cd6d5e11
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-08-12 10:42:04 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("head.php");?>
+<body>
+  <?php include_once("nav.php");?>
+  <main>
+    <div class="container">
+        
+    </div>
+  </main>
+</body>
+</html>
