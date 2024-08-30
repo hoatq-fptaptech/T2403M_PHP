75e3fba2ebf81222b699a83d1222944c8932624d
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-08-30 09:01:17 +0700

    product admin

--- html/admin_aside.php
+++ html/admin_aside.php
@@ -1,8 +1,8 @@
 <aside class="col-3">
                 <ul class="list-group">
-                    <li class="list-group-item">Orders</li>
+                    <li class="list-group-item"><a href="/admin/">Orders</a></li>
                     <li class="list-group-item">Customers</li>
-                    <li class="list-group-item">Products</li>
+                    <li class="list-group-item"><a href="/admin/product.php">Products</a></li>
                     <li class="list-group-item">Categories</li>
                     <li class="list-group-item">Reviews</li>
                 </ul>
